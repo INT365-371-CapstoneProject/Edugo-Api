29b6d8e08c442758f9f88fe29dfe1c6ec687988c
TECHATHAM KANJANASUTTAKUL <techatham.neng@mail.kmutt.ac.th>
2024-11-16 20:00:15 +0700

    Update insert.sql

--- ScirptData/insert.sql
+++ ScirptData/insert.sql
@@ -10,7 +10,7 @@ INSERT INTO `edugo`.`countries` (`country_id`, `name`) VALUES
 -- Insert into posts
 INSERT INTO `edugo`.`posts` (`posts_id`, `title`, `description`, `image`, `publish_date`, `posts_type`, `country_id`) VALUES
 (1, 'New Announcement', 'This is a new announcement for all users', NULL, NOW(), 'Announce', 1),
-(2, 'Subject Update', 'An update on the subject matter', NULL, NOW(), 'Announce', 2);
+(2, 'Subject Update', 'An update on the subject matter', NULL, "2024-11-10 12:29:29", 'Announce', 2);
 
 -- Insert into comments
 INSERT INTO `edugo`.`comments` (`comments_id`, `comments_text`, `comments_image`, `comments_type`, `publish_date`, `posts_id`) VALUES
@@ -28,4 +28,4 @@ INSERT INTO `edugo`.`categories` (`category_id`, `name`) VALUES
 -- Insert into announce_posts
 INSERT INTO `edugo`.`announce_posts` (`announce_id`, `url`, `attach_file`, `close_date`, `posts_id`, `category_id`) VALUES
 (1, 'https://example.com/announcement1', NULL, "2024-12-20 12:29:29", 1, 1),
-(2, 'https://example.com/announcement2', NULL, "2024-11-10 12:29:29", 2, 2);
+(2, 'https://example.com/announcement2', NULL, "2024-11-15 12:29:29", 2, 2);
