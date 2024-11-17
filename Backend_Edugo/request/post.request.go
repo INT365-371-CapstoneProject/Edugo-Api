ba9cd366b0895eace2c39b3ca9fb228f6f04cee5
TECHATHAM KANJANASUTTAKUL <techatham.neng@mail.kmutt.ac.th>
2024-11-18 00:26:23 +0700

    Update post.request.go

--- Backend_Edugo/request/post.request.go
+++ Backend_Edugo/request/post.request.go
@@ -3,26 +3,26 @@ package request
 import "time"
 
 type AnnouncePostCreateRequest struct {
-	Title        string     `json:"title" validate:"required"`
-	Description  string     `json:"description" validate:"required"`
+	Title        string     `json:"title" validate:"required" message:"Title is required"`
+	Description  string     `json:"description" validate:"required" message:"Description is required"`
 	URL          *string    `json:"url"`
 	Attach_File  *string    `json:"attach_file"`
 	Image        *string    `json:"image"`
-	Posts_Type   string     `json:"posts_type" validate:"required,oneof=Announce Subject"`
+	Posts_Type   string     `json:"posts_type" validate:"required,oneof=Announce Subject" message:"Posts_Type is required and must be either 'Announce' or 'Subject'"`
 	Publish_Date *time.Time `json:"published_date"`
-	Close_Date   *time.Time `json:"closed_date" validate:"required"`
-	Category_ID  uint       `json:"category_id" validate:"required"`
-	Country_ID   uint       `json:"country_id" validate:"required"`
+	Close_Date   *time.Time `json:"closed_date" validate:"required" message:"Close_Date is required"`
+	Category_ID  uint       `json:"category_id" validate:"required" message:"Category_ID is required"`
+	Country_ID   uint       `json:"country_id" validate:"required" message:"Country_ID is required"`
 }
 
 type AnnouncePostUpdateRequest struct {
-	Title        string     `json:"title" validate:"required"`
-	Description  string     `json:"description" validate:"required"`
+	Title        string     `json:"title" validate:"required" message:"Title is required"`
+	Description  string     `json:"description" validate:"required" message:"Description is required"`
 	URL          *string    `json:"url"`
 	Attach_File  *string    `json:"attach_file"`
 	Image        *string    `json:"image"`
 	Publish_Date *time.Time `json:"published_date"`
-	Close_Date   *time.Time `json:"closed_date" validate:"required"`
-	Category_ID  uint       `json:"category_id" validate:"required"`
-	Country_ID   uint       `json:"country_id" validate:"required"`
+	Close_Date   *time.Time `json:"closed_date" validate:"required" message:"Close_Date is required"`
+	Category_ID  uint       `json:"category_id" validate:"required" message:"Category_ID is required"`
+	Country_ID   uint       `json:"country_id" validate:"required" message:"Country_ID is required"`
 }
