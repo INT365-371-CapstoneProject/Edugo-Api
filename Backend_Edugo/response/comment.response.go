aa90950f197bb559a3ab0fbba4c9e4701864a2ab
Puntakan <puntakanc@gmail.com>
2025-03-19 15:40:57 +0700

    FIx code
    Add GetAllComment

--- Backend_Edugo/response/comment.response.go
+++ Backend_Edugo/response/comment.response.go
@@ -5,9 +5,9 @@ import (
 )
 
 type CommentResponse struct {
-	Comments_ID     uint      `json:"id"`
-	Comments_Text  string    `json:"comments_text"`
-	Publish_Date   time.Time `json:"publish_date"`
-	Posts_ID        uint      `json:"post_id"`
-	Account_ID     uint      `json:"account_id"`
+	Comments_ID   uint      `json:"id"`
+	Comments_Text string    `json:"comments_text"`
+	Publish_Date  time.Time `json:"publish_date"`
+	Posts_ID      uint      `json:"post_id"`
+	Account_ID    uint      `json:"account_id"`
 }
